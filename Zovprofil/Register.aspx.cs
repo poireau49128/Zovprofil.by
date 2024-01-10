b08ae4a9e5e4108af424a29681f739a66988c01c
poireau49128 <kovalkov1899@mail.ru>
2024-01-10 11:15:35 +0300

    Добавлена рабочая авторизация на стандартной модели + стандартный хэшер

--- Zovprofil/Register.aspx.cs
+++ Zovprofil/Register.aspx.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 using System;
 using System.Linq;
+using System.Web;
 
 namespace Zovprofil
 {
@@ -12,13 +14,16 @@ namespace Zovprofil
             // Default UserStore constructor uses the default connection string named: DefaultConnection
             var userStore = new UserStore<IdentityUser>();
             var manager = new UserManager<IdentityUser>(userStore);
-
             var user = new IdentityUser() { UserName = UserName.Text };
+
             IdentityResult result = manager.Create(user, Password.Text);
 
             if (result.Succeeded)
             {
-                StatusMessage.Text = string.Format("User {0} was created successfully!", user.UserName);
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = manager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+                Response.Redirect("~/Login.aspx");
             }
             else
             {
